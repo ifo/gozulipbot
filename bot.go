7b6e4904481c0b7e4956a54e1303cdf419356645
Steve McCarthy <steve@redlua.com>
2016-06-13 01:34:52 -0400

    Move queue to its own file

--- bot.go
+++ bot.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"net/url"
-	"strconv"
 	"strings"
 )
 
@@ -20,13 +18,6 @@ type Bot struct {
 
 type Doer interface {
 	Do(*http.Request) (*http.Response, error)
-}
-
-type Queue struct {
-	ID           string `json:"queue_id"`
-	LastEventID  int    `json:"last_event_id"`
-	MaxMessageID int    `json:"max_message_id"`
-	bot          *Bot   `json:"-"`
 }
 
 // Init adds an http client to an existing bot struct.
@@ -216,43 +207,4 @@ func (b *Bot) constructRequest(method, endpoint, body string) (*http.Request, er
 	req.SetBasicAuth(b.Email, b.APIKey)
 
 	return req, nil
-}
-
-// GetEvents is a blocking call that waits for and parses a list of EventMessages.
-// There will usually only be one EventMessage returned.
-func (q *Queue) GetEvents() ([]EventMessage, error) {
-	resp, err := q.RawGetEvents()
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	msgs, err := ParseEventMessages(body)
-	if err != nil {
-		return nil, err
-	}
-
-	return msgs, nil
-}
-
-// RawGetEvents is a blocking call that receives a response containing a list
-// of events (a.k.a. received messages) since the last message id in the queue.
-func (q *Queue) RawGetEvents() (*http.Response, error) {
-	values := url.Values{}
-	values.Set("queue_id", q.ID)
-	values.Set("last_event_id", strconv.Itoa(q.LastEventID))
-
-	url := "events?" + values.Encode()
-
-	req, err := q.bot.constructRequest("GET", url, "")
-	if err != nil {
-		return nil, err
-	}
-
-	return q.bot.client.Do(req)
 }
---
+++ queue.go
@@ -0,0 +1,54 @@
+package gozulipbot
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strconv"
+)
+
+type Queue struct {
+	ID           string `json:"queue_id"`
+	LastEventID  int    `json:"last_event_id"`
+	MaxMessageID int    `json:"max_message_id"`
+	bot          *Bot   `json:"-"`
+}
+
+// GetEvents is a blocking call that waits for and parses a list of EventMessages.
+// There will usually only be one EventMessage returned.
+func (q *Queue) GetEvents() ([]EventMessage, error) {
+	resp, err := q.RawGetEvents()
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	msgs, err := ParseEventMessages(body)
+	if err != nil {
+		return nil, err
+	}
+
+	return msgs, nil
+}
+
+// RawGetEvents is a blocking call that receives a response containing a list
+// of events (a.k.a. received messages) since the last message id in the queue.
+func (q *Queue) RawGetEvents() (*http.Response, error) {
+	values := url.Values{}
+	values.Set("queue_id", q.ID)
+	values.Set("last_event_id", strconv.Itoa(q.LastEventID))
+
+	url := "events?" + values.Encode()
+
+	req, err := q.bot.constructRequest("GET", url, "")
+	if err != nil {
+		return nil, err
+	}
+
+	return q.bot.client.Do(req)
+}
