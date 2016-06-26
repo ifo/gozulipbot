dc7a8325f98c095d2d4018035efd5d499ebdcc50
Steve McCarthy <steve@redlua.com>
2016-06-26 01:32:37 -0400

    Test Bot.PrivateMessage

--- message_test.go
+++ message_test.go
@@ -11,5 +11,39 @@ func TestMessage(t *testing.T) {
 }
 
 func TestPrivateMessage(t *testing.T) {
-	t.Skip()
+	bot := getTestBot()
+	type C struct {
+		M    Message
+		Body string
+		E    error
+	}
+	cases := map[string]C{
+		"1": C{M: Message{Stream: "a", Emails: []string{"a@example.com"}, Content: "hey"}, // normal
+			Body: "content=hey&to=a%40example.com&type=private", E: nil},
+		"2": C{M: Message{Stream: "a", Topic: "a", Emails: []string{"a@example.com"}, Content: "hey"}, // topic is ignored
+			Body: "content=hey&to=a%40example.com&type=private", E: nil},
+		"3": C{M: Message{Stream: "a", Content: "hey"}, // no email set
+			Body: "", E: errors.New("there must be at least one recipient")},
+	}
+
+	for num, c := range cases {
+		// ignore response from testClient
+		_, err := bot.PrivateMessage(c.M)
+
+		body, _ := ioutil.ReadAll(bot.Client.(*testClient).Request.Body)
+		if string(body) != c.Body {
+			t.Errorf("got %q, expected %q, case %q", string(body), c.Body, num)
+		}
+
+		// no error expected
+		if c.E == nil && err != nil {
+			t.Errorf("got %q, expected nil, case %q", err, num)
+		}
+		// error expected, prevent nil.Error() panic
+		if c.E != nil && err == nil {
+			t.Errorf("got nil, expected %q, case %q", err, c.E, num)
+		} else if c.E != nil && err.Error() != c.E.Error() {
+			t.Errorf("got %q, expected %q, case %q", err, c.E, num)
+		}
+	}
 }
